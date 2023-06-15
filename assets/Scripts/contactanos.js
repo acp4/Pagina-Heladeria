9994f57f11dc5709f2dce6620ac3728b0172f9e2
Jessica Sanchez <jessamesan@hotmail.com>
2023-06-15 17:42:10 -0600

    Tercer intento

--- assets/Scripts/contactanos.js
+++ assets/Scripts/contactanos.js
@@ -1,17 +1,23 @@
+
+const jsConfetti = new JSConfetti();
+
+// Define la función confetti
 const confetti = async () => {
-    try {
-      await jsConfetti.addConfetti({
-        emojis: ['🍧', '🍦', '🍨'],
-        emojiSize: 50,
-        confettiNumber: 50,
-      });
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    await jsConfetti.addConfetti({
+      emojis: ['🍧', '🍦', '🍨'],
+      emojiSize: 50,
+      confettiNumber: 50,
+    });
+  } catch (error) {
+    console.error(error);
   }
- 
-  
-function envioForma() {
-confetti();
-setTimeout(() => { window.location.href = "../../index.html" }, 2000);
 };
+
+function envioForma() {
+  confetti();
+  setTimeout(() => { window.location.href = "../../index.html" }, 2000);
+}
+
+const botonEnviar = document.getElementById('boton-enviar-contacto');
+botonEnviar.onclick = envioForma;
